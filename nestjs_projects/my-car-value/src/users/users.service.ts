6339e7440dc86d6c8333d20a29dc14e8aa294f17
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2021-12-27 09:29:12 +0530

    Update users.service.ts

--- nestjs_projects/my-car-value/src/users/users.service.ts
+++ nestjs_projects/my-car-value/src/users/users.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository) {}
+}
