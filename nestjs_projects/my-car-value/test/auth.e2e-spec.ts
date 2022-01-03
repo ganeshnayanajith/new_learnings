694d29ac8d1c9f1587beed29ab91c7e62c92ca10
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-01-03 20:39:03 +0530

    setup file for db deletion before every test

--- nestjs_projects/my-car-value/test/auth.e2e-spec.ts
+++ nestjs_projects/my-car-value/test/auth.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Authentication System', () => {
   });
 
   it('handles a signup request', () => {
-    const email = 'teste4@test.com';
+    const email = 'teste@test.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: 'passworde1' })
