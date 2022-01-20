67fa06d7a533acde995cf03bbc6d76d568c91e4d
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-01-20 11:26:08 +0530

    remove cache

---
+++ nestjs_projects/messages/src/messages/messages.service.ts
@@ -0,0 +1,25 @@
+import { MessagesRepository } from './messages.repository';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MessagesService {
+  /*messagesRepo: MessagesRepository;
+
+  constructor() {
+    this.messagesRepo = new MessagesRepository();
+  }*/
+
+  constructor(public messagesRepo: MessagesRepository) {}
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
---
+++ serverless_projects/tutorial-aws-lambda/python-environment-variables-example/handler.py
@@ -0,0 +1,6 @@
+import json
+import os
+
+
+def hello(event, context):
+    return os.environ['FIRST_NAME']
