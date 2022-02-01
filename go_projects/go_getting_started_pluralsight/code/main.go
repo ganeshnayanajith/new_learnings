50b321ebc706a938d76d226a7d17f622930ac92e
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-01 23:34:59 +0530

    0201

--- go_projects/go_getting_started_pluralsight/code/main.go
+++ go_projects/go_getting_started_pluralsight/code/main.go
@@ -2,6 +2,72 @@ package main
 
 import "fmt"
 
+const (
+	/* first  = iota + 6
+	second = 2 << iota */
+
+	first = iota + 6
+	second
+)
+
 func main() {
-	fmt.Println("Hello")
+
+	fmt.Println("Program running..")
+	fmt.Println(first, second)
+
+	/* const c int = 3
+	fmt.Println(c + 3)
+	fmt.Println(float32(c) + 1.2) */
+
+	/* const c int = 3
+	fmt.Println(c + 3)
+	fmt.Println(c + 1.2) */
+
+	/* const c = 3
+	fmt.Println(c + 3)
+	fmt.Println(c + 1.2) */
+
+	/* const pi = 3.1415
+	fmt.Println(pi)
+	// pi = 2.5 */
+
+	/* firstName := "Ganesh"
+	fmt.Println(firstName)
+
+	ptr := &firstName
+	fmt.Println(ptr, *ptr)
+
+	firstName = "Nayanajith"
+	fmt.Println(ptr, *ptr) */
+
+	/* firstName := "Ganesh"
+	fmt.Println(firstName)
+
+	ptr := &firstName
+	fmt.Println(ptr, *ptr) */
+
+	/* var firstName *string = new(string)
+	*firstName = "Ganesh"
+	fmt.Println(firstName)
+	fmt.Println(*firstName) */
+
+	/* var i int
+	i = 42
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	firstName := "Ganesh"
+	fmt.Println(firstName)
+
+	b := true
+	fmt.Println(b)
+
+	c := complex(3, 4)
+	fmt.Println(c)
+
+	r, im := real(c), imag(c)
+	fmt.Println(r, im) */
+
 }
