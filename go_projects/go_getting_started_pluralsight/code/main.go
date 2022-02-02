a7cc7517c92d8dc576ad81cdc9161b418a589fc7
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-02 20:15:42 +0530

    0202

--- go_projects/go_getting_started_pluralsight/code/main.go
+++ go_projects/go_getting_started_pluralsight/code/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"ganeshnayanajith/new_learnings/tree/main/go_projects/go_getting_started_pluralsight/code/models"
+)
 
 func main() {
 
 	fmt.Println("Program running..")
+
+	u := models.User{
+		ID:        2,
+		FirstName: "Ganesh",
+		LastName:  "Nayanajith",
+	}
+
+	fmt.Println(u)
 
 }
