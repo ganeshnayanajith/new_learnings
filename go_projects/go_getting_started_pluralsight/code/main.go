abe97fae620855ab6d35929c8188a041b4589a70
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-04 23:18:02 +0530

    web server done

--- go_projects/go_getting_started_pluralsight/code/main.go
+++ go_projects/go_getting_started_pluralsight/code/main.go
@@ -1,8 +1,17 @@
 package main
 
+import (
+	"ganeshnayanajith/new_learnings/tree/main/go_projects/go_getting_started_pluralsight/code/controllers"
+	"net/http"
+)
+
+type HTTPRequest struct {
+	Method string
+}
+
 func main() {
 
-	/* controllers.RegisterControllers()
-	http.ListenAndServe(":3000", nil) */
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 
 }
--- go_projects/go_getting_started_pluralsight/code/models/user.go
+++ go_projects/go_getting_started_pluralsight/code/models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type User struct {
 	ID        int
@@ -25,4 +28,33 @@ func AddUser(u User) (User, error) {
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
+}
+
+func RemoveUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
