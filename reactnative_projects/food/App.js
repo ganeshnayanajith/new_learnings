3bf82e9a0e8a0bfd463eac5b7844e3590168ef38
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-11 00:27:33 +0530

    react native restaurant app init

---
+++ reactnative_projects/food/App.js
@@ -0,0 +1,22 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi kakka bole</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
