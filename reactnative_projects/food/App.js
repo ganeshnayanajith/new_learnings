8f3bc5ceb56e89f48c11859ae6e8395cf456bf09
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-11 22:00:50 +0530

    add search screen and bar with small styles

--- reactnative_projects/food/App.js
+++ reactnative_projects/food/App.js
@@ -1,22 +1,14 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hi kakka bole</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Business Search'
+  }
 });
+
+export default createAppContainer(navigator);
