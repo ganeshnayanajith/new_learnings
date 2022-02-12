2381e51cef6fbcc709bf330fce2ee3135e4f607c
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-12 22:45:16 +0530

    added error handling

--- reactnative_projects/food/src/screens/SearchScreen.js
+++ reactnative_projects/food/src/screens/SearchScreen.js
@@ -6,17 +6,23 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchAPI = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses);
-  }
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('something went wrong');
+    }
+
+  };
 
   return (
     <View>
@@ -25,7 +31,7 @@ const SearchScreen = () => {
         onTermChange={setTerm}
         onTermSubmit={searchAPI}
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
