6903adf0d14f37dee66a0e9d1c271b788b2b05ef
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-12 20:12:11 +0530

    add search term state management

--- reactnative_projects/food/src/components/SearchBar.js
+++ reactnative_projects/food/src/components/SearchBar.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
-    <View style={styles.background}>
-      <Text> Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle}/>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+        // onChangeText={newTerm => onTermChange(newTerm)}
+        // onEndEditing={() => onTermSubmit()}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
+    marginTop: 10,
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    // borderColor: 'black',
+    // borderWidth: 1,
+    fontSize: 18,
+    flex: 1
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
     marginHorizontal: 15
   }
 });
--- reactnative_projects/food/src/screens/SearchScreen.js
+++ reactnative_projects/food/src/screens/SearchScreen.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
   return (
     <View>
-      <SearchBar/>
-      <Text> Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={newTerm => setTerm(newTerm)}
+        onTermSubmit={() => console.log('term was submitted')}
+      />
+      <Text>Search Screen</Text>
+      <Text>{term}</Text>
     </View>
   );
 };
