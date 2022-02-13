c55fcf8f99dd73e1028d7cf77f2decd6f45a2f03
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-13 13:43:37 +0530

    show different results lists

---
+++ reactnative_projects/udemy_complete_react_native_plus_hooks_course/food/src/components/ResultsList.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const ResultsList = ({ title }) => {
+  return (
+    <View>
+      <Text style={styles.titleStyle}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
+
+export default ResultsList;
