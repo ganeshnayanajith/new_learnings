afc5f884dee5aaef17cb2308a7bd798bc247acab
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-13 12:50:30 +0530

    0213

---
+++ reactnative_projects/udemy_complete_react_native_plus_hooks_course/food/src/screens/SearchScreen.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+
+const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  const [searchAPI, results, errorMessage] = useResults();
+
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchAPI(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
