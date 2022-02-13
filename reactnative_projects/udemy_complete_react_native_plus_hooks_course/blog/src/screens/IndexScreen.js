50a3e0a4fcbc7787a1c39323fc526fcd1697055f
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-13 23:37:07 +0530

    added index screen and provider wrapper

---
+++ reactnative_projects/udemy_complete_react_native_plus_hooks_course/blog/src/screens/IndexScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
