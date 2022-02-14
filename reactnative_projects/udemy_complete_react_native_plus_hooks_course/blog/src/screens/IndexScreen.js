959a056e7ea9e433072b5a852eb2ed88aab454a2
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-14 19:31:17 +0530

    communicate data using context and change state of the context provider

--- reactnative_projects/udemy_complete_react_native_plus_hooks_course/blog/src/screens/IndexScreen.js
+++ reactnative_projects/udemy_complete_react_native_plus_hooks_course/blog/src/screens/IndexScreen.js
@@ -1,14 +1,17 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 import { useContext } from 'react';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const blogPosts = useContext(BlogContext);
+  const { data, addBlogPost } = useContext(BlogContext);
+  console.log('rendering...');
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost}/>
+      {/*<Button title="Add Post" onPress={() => addBlogPost()}/>*/}
       <FlatList
-        data={blogPosts}
+        data={data}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return (
