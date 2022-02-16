be9dda09f0c2ebdf110f717dd6c0b7140f7b1e4f
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-16 21:34:58 +0530

    list blog post with delete icon

--- reactnative_projects/udemy_complete_react_native_plus_hooks_course/blog/src/screens/IndexScreen.js
+++ reactnative_projects/udemy_complete_react_native_plus_hooks_course/blog/src/screens/IndexScreen.js
@@ -1,13 +1,13 @@
 import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 import { useContext } from 'react';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
   console.log('rendering...');
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button title="Add Post" onPress={addBlogPost}/>
       {/*<Button title="Add Post" onPress={() => addBlogPost()}/>*/}
       <FlatList
@@ -15,7 +15,10 @@ const IndexScreen = () => {
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <Text>{item.title}</Text>
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.icon} name="trash"/>
+            </View>
           )
         }}
       />
@@ -23,6 +26,21 @@ const IndexScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray'
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
