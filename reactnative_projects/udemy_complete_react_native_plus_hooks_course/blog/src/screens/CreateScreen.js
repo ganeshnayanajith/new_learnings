26705a5b271d03ca843b89a9de2d114b615ae1bb
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-02-16 23:37:59 +0530

    create screen and header navigation

---
+++ reactnative_projects/udemy_complete_react_native_plus_hooks_course/blog/src/screens/CreateScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { useContext } from 'react';
+import { Context } from '../context/BlogContext';
+
+const CreateScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
