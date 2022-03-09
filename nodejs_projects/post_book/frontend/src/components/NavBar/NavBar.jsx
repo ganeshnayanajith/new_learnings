fb150bf315147da1a08a17f9b3aa413cbe40c121
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-03-09 06:52:32 +0530

    navbar

--- nodejs_projects/post_book/frontend/src/components/NavBar/NavBar.jsx
+++ nodejs_projects/post_book/frontend/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 
 let NavBar = () => {
@@ -7,12 +6,39 @@ let NavBar = () => {
     <React.Fragment>
       <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
         <div className="container">
-          <Link to={'/settings'} className="nav-link"><i className="fa fa-gear" /></Link>
-          <Link to={'/notifications'} className="nav-link"><i className="fa fa-bell" /></Link>
-          <Link to={'/messages'} className="nav-link"><i className="fa fa-message" /></Link>
-          <Link to={'/home'} className="nav-link"><img src={logo} alt="Logo" height={30}
-                                                       style={{ backgroundColor: 'white', height: '40px' }} /></Link>
-          <Link to={'/profile'} className="nav-link">Ganesh Nayanajith <i className="fa fa-user" /></Link>
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#"><i className="fa fa-gear" /></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#"><i className="fa fa-bell" /></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#"><i className="fa fa-message" /></a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <img src={logo} alt="Logo" height={30}
+                     style={{ backgroundColor: 'white', height: '40px' }} />
+              </li>
+            </ul>
+          </div>
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Ganesh Nayanajith <i className="fa fa-user" /></a>
+              </li>
+            </ul>
+          </div>
+
+
         </div>
       </nav>
     </React.Fragment>
