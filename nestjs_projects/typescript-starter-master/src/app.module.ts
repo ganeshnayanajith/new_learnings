a7fdd25ae78873d53e90f278e634ac72503bd10f
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-03-25 05:20:19 +0530

    authentication and users

--- nestjs_projects/typescript-starter-master/src/app.module.ts
+++ nestjs_projects/typescript-starter-master/src/app.module.ts
@@ -3,8 +3,8 @@ import { PostsModule } from './posts/posts.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
 import { DatabaseModule } from './database/database.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   imports: [
@@ -17,11 +17,15 @@ import { AppService } from './app.service';
         POSTGRES_PASSWORD: Joi.string().required(),
         POSTGRES_DB: Joi.string().required(),
         PORT: Joi.number(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
       }),
     }),
     DatabaseModule,
+    UsersModule,
+    AuthenticationModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
