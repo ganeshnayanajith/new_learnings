2ed7a01ce33658c0a8f25de5bb03a918f2d69c4f
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2022-04-17 11:39:21 +0530

    added firebase.config.json

--- reactjs_projects/ReactFirebaseAuth/src/firebase.js
+++ reactjs_projects/ReactFirebaseAuth/src/firebase.js
@@ -1,31 +1,25 @@
 import { initializeApp } from "firebase/app";
 import {
-  GoogleAuthProvider,
-  getAuth,
-  signInWithPopup,
-  signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
   sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
 } from "firebase/auth";
-import {
-  getFirestore,
-  query,
-  getDocs,
-  collection,
-  where,
-  addDoc,
-} from "firebase/firestore";
+import { addDoc, collection, getDocs, getFirestore, query, where, } from "firebase/firestore";
 
+import config from './firebase.config.json';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_PBvnRHEQvYxxWNbBfD5Jtu2GbHknNng",
-  authDomain: "fir-auth-article-c3c77.firebaseapp.com",
-  projectId: "fir-auth-article-c3c77",
-  storageBucket: "fir-auth-article-c3c77.appspot.com",
-  messagingSenderId: "1034810777878",
-  appId: "1:1034810777878:web:b609b38aa9bbc225a7f037",
-  measurementId: "G-J5QWTQ0CER"
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
@@ -37,6 +31,7 @@ const googleProvider = new GoogleAuthProvider();
 const signInWithGoogle = async () => {
   try {
     const res = await signInWithPopup(auth, googleProvider);
+    console.log(await res.user.getIdToken(true));
     const user = res.user;
     const q = query(collection(db, "users"), where("uid", "==", user.uid));
     const docs = await getDocs(q);
